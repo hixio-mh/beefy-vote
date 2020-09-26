50f3a3e519224284a3f8b54c760f00b3d3af4360
superbeefyboy <superbeefyboy@pm.me>
2020-09-26 14:26:27 -0300

    simplified asset management

--- src/components/Token.vue
+++ src/components/Token.vue
@@ -1,7 +1,7 @@
 <template>
   <img
     :src="url"
-    class="d-inline-block bg-gray-9 v-align-middle line-height-0 circle border"
+    class="d-inline-block v-align-middle line-height-0"
     :style="{
       width: `${size || 22}px`,
       height: `${size || 22}px`
@@ -14,12 +14,7 @@ export default {
   props: ['space', 'size', 'symbolIndex'],
   computed: {
     url() {
-      const file = this.symbolIndex
-        ? this.symbolIndex === 'space'
-          ? 'space'
-          : `logo${this.symbolIndex}`
-        : 'logo';
-      return `https://raw.githubusercontent.com/bonustrack/snapshot-spaces/master/spaces/${this.space}/${file}.png`;
+      return require(`@/spaces/${this.space}/logo.png`);
     }
   }
 };
