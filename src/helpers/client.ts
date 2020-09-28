02287c1d07dc4c48d7bf24cec46ac554587e5fa6
superbeefyboy <superbeefyboy@pm.me>
2020-09-28 12:51:27 -0300

    cors wip

--- src/helpers/client.ts
+++ src/helpers/client.ts
@@ -1,25 +1,31 @@
 class Client {
   request(command, body?) {
-    const url = `${process.env.VUE_APP_HUB_URL}/api/${command}`;
-    let init;
+    console.debug(`>>> Client.request`, command, body);
 
-    console.debug(`>>> Client.request`, url, command, body);
+    const url = `${process.env.VUE_APP_HUB_URL}/api/${command}`;
+    const init: any = {
+      mode: 'cors',
+      cache: 'default',
+    };
 
     if (body) {
-      init = {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(body),
+      init.method = 'POST';
+      init.headers = {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
       };
+      init.body = JSON.stringify(body);
+    } else {
+      init.method = 'GET';
     }
+
+    console.debug(`>>> preparing request`, url, init);
+    const req = new Request(url, init);
+
     return new Promise((resolve, reject) => {
-      fetch(url, init)
+      fetch(req)
         .then(res => {
           console.debug(`>>> response`, res);
-          
           if (res.ok) {
             return resolve(res.json());
           }
