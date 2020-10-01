56b4e78f1c896e2750d7385720471841f095b3fe
superbeefyboy <superbeefyboy@pm.me>
2020-10-01 09:20:17 -0300

    BSC has no ws endpoint yet

--- src/helpers/ws.ts
+++
@@ -1,9 +0,0 @@
-import { WebSocketProvider } from '@ethersproject/providers';
-import config from '@/helpers/config';
-
-let provider;
-const chainId = '56';
-const url: any = config.networks[chainId].ws_url;
-if (url) provider = new WebSocketProvider(url);
-
-export default provider;
