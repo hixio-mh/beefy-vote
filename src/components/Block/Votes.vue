0bcbfbb739d7629ee5fbc2056f6924d84831da79
superbeefyboy <superbeefyboy@pm.me>
2020-10-01 20:43:30 -0300

    improved vote results

--- src/components/Block/Votes.vue
+++ src/components/Block/Votes.vue
@@ -1,27 +1,27 @@
 <template>
   <Block
-    v-if="Object.keys(votes).length > 0"
+    v-if="sortedVotes.length > 0"
     title="Votes"
-    :counter="Object.keys(votes).length"
+    :counter="sortedVotes.length"
     :slim="true"
   >
     <div
-      v-for="(vote, address, i) in visibleVotes"
+      v-for="(kv, i) in visibleVotes"
       :key="i"
       :style="i === 0 && 'border: 0 !important;'"
       class="px-4 py-3 border-top d-flex"
     >
-      <User :address="address" :space="space" class="column" />
+      <User :address="kv[0]" :space="space" class="column" />
       <div
-        v-text="proposal.msg.payload.choices[vote.msg.payload.choice - 1]"
+        v-text="proposal.msg.payload.choices[kv[1].msg.payload.choice - 1]"
         class="flex-auto text-center text-white"
       />
       <div class="column text-right text-white">
-        <span class="tooltipped tooltipped-n" :aria-label="vote.score">
-          {{ `${_numeral(vote.score)} ${_shorten(space.symbol, 'symbol')}` }}
+        <span class="tooltipped tooltipped-n" :aria-label="kv[1].score">
+          {{ `${_numeral(kv[1].score)} ${_shorten(space.symbol, 'symbol')}` }}
         </span>
         <a
-          @click="openReceiptModal(vote)"
+          @click="openReceiptModal(kv[1])"
           target="_blank"
           class="ml-2 text-gray"
           title="Receipt"
@@ -31,7 +31,7 @@
       </div>
     </div>
     <a
-      v-if="!showAllVotes && Object.keys(votes).length > 10"
+      v-if="!showAllVotes && sortedVotes.length > 10"
       @click="showAllVotes = true"
       class="px-4 py-3 border-top text-center d-block header-bg"
     >
@@ -58,10 +58,11 @@ export default {
     };
   },
   computed: {
+    sortedVotes() {
+      return Object.entries(this.votes).sort((a, b) => b[1].score - a[1].score);
+    },
     visibleVotes() {
-      return this.showAllVotes
-        ? this.votes
-        : Object.fromEntries(Object.entries(this.votes).slice(0, 10));
+      return this.showAllVotes ? this.sortedVotes : this.sortedVotes.slice(0, 10);
     },
     titles() {
       return [this.space.symbol];
