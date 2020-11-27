271a325ccf1f0c12bb73eaea499d939e30b2ac5c
superbeefyboy <superbeefyboy@pm.me>
2020-11-27 05:47:27 -0300

    trustwallet

--- src/auth.ts
+++ src/auth.ts
@@ -1,10 +1,12 @@
 import Vue from 'vue';
-import { LockPlugin } from '@bonustrack/lock/plugins/vue';
-import injected from '@bonustrack/lock/connectors/injected';
+import { LockPlugin } from '@snapshot-labs/lock/plugins/vue';
+import injected from '@snapshot-labs/lock/connectors/injected';
+import walletconnect from '@snapshot-labs/lock/connectors/walletconnect';
+
 import config from '@/helpers/config';
 
 const options: any = { connectors: [] };
-const connectors = { injected };
+const connectors = { injected, walletconnect };
 
 Object.entries(config.connectors).forEach((connector: any) => {
   options.connectors.push({
